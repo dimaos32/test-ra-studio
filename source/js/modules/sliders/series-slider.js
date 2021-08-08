3a679f0699c22eb653da81666ed66626056a0715
Dima Os <dimaos@protonmail.com>
2021-08-08 18:56:14 +0300

    Добавляет адаптив

--- source/js/modules/sliders/series-slider.js
+++ source/js/modules/sliders/series-slider.js
@@ -10,23 +10,33 @@ const initSeriesSlider = () => {
     const totalPages = slider.querySelector('.series-slider__fraction--total-pages');
 
     const swiper = new Swiper('.series-slider__wrapper', {
-      slidesPerView: 3,
-      slidesPerGroup: 3,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
       spaceBetween: 35,
       speed: 600,
       scrollbar: {
         el: '.series-slider__scrollbar',
       },
+      breakpoints: {
+        600: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+        },
+        992: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+        },
+      },
     });
 
-    totalPages.textContent = Math.ceil(swiper.slides.length / 3) < 10 ?
-      '0' + Math.ceil(swiper.slides.length / 3) :
-      Math.ceil(swiper.slides.length / 3);
+    totalPages.textContent = Math.ceil(swiper.slides.length / swiper.passedParams.slidesPerGroup) < 10 ?
+      '0' + Math.ceil(swiper.slides.length / swiper.passedParams.slidesPerGroup) :
+      Math.ceil(swiper.slides.length / swiper.passedParams.slidesPerGroup);
 
     swiper.on('slideChange', () => {
-      const newCurrentSlide = Math.ceil(swiper.realIndex / 3 + 1) < 10 ?
-        '0' + Math.ceil(swiper.realIndex / 3 + 1) :
-        Math.ceil(swiper.realIndex / 3 + 1);
+      const newCurrentSlide = Math.ceil(swiper.realIndex / swiper.passedParams.slidesPerGroup + 1) < 10 ?
+        '0' + Math.ceil(swiper.realIndex / swiper.passedParams.slidesPerGroup + 1) :
+        Math.ceil(swiper.realIndex / swiper.passedParams.slidesPerGroup + 1);
       currentPage.textContent = newCurrentSlide;
     });
   });
